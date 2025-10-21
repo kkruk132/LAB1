45d7bb4f407ebc269f4c535ff1c701d493a85985
kkruk132 <73144@student.wsiz.edu.pl>
2025-10-21 20:58:39 +0200

    Dodanie dodatkowych zadań

---
+++ zad9.py
@@ -0,0 +1,20 @@
+ #Zad 9
+
+
+#Wczytywanie zmiennych od użytkownika
+a = float(input("Podaj pierwszą liczbę:"))
+b = float(input("Podaj drugą liczbę:"))
+
+ #działania na podanych zmiennych
+dodawanie = a + b
+odejmowanie = a - b
+mnozenie = a * b
+dzielenie = a / b
+potegowanie = a ** b
+
+#drukowanie wyników
+print("Wynik dodawania podanych liczb to:",dodawanie)
+print("Wynik odejmowania podanych liczb to:",odejmowanie)
+print("Wynik mnożenia podanych liczb to:",mnozenie)
+print("Wynik dzielenia podanych liczb to:",dzielenie)
+print("Wynik potęgowania podanych liczb to:",potegowanie)
